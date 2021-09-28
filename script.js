1ce4e879b2365f66762eb718ad1da179b49d6b72
rishabh150502 <itsrk1505@gmail.com>
2021-09-28 16:16:44 +0530

    adding book info in array

--- script.js
+++ script.js
@@ -17,15 +17,18 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
+//Book info
 const content=document.querySelector('.content');
+function displayBooks(){
+    for(let i=0;i<myLibrary.length;i++){
 const bookDiv=document.createElement('div');
 const optionDiv=document.createElement('div');
 const readButton=document.createElement('button');
 const deleteButton=document.createElement('button');
-bookDiv.textContent=`\n\tAuthor: \t     ${myLibrary[0].author}
-                     \n\tTitle:  \t     ${myLibrary[0].title}
-                     \n\tPages:         ${myLibrary[0].pages}
-                     \n\tMark as Read:  ${myLibrary[0].markRead}`;
+bookDiv.textContent=`\n\tAuthor: \t     ${myLibrary[i].author}
+                     \n\tTitle:  \t     ${myLibrary[i].title}
+                     \n\tPages:         ${myLibrary[i].pages}
+                     \n\tMark as Read:  ${myLibrary[i].markRead}`;
 readButton.textContent=`Marked as Read`;
 deleteButton.textContent=`Delete`;
 readButton.classList.add('read');
@@ -36,3 +39,53 @@ optionDiv.append(readButton);
 optionDiv.append(deleteButton);
 bookDiv.append(optionDiv);
 content.append(bookDiv);
+    }
+}
+
+//form 
+const form=document.querySelector('.form');
+
+const addBookButton=document.querySelector('.add');
+
+addBookButton.addEventListener('click',function(){
+    if(addBookButton.textContent==='Add Book'){
+form.innerHTML=`<div>
+<label for="bookName">Book Title</label>
+<input type="text" id="bookName" name="bookName" required>
+</div>
+<div>
+<label for="authorName">Author Name</label>
+<input type="text" id="authorName" name="authorName" required>
+</div>
+<div>
+<label for="pages">Number Of Pages</label>
+<input type="number" id="pages" name="pages" required>
+</div>
+<div>
+<input type="checkbox" id="markRead" name="markRead" value="Read">
+<label for="markRead"> Mark book as read</label>
+</div>
+<button class="addBook" type="submit" form="form1" value="Submit">Add</button>`;
+addBookButton.textContent="Cancel"
+    }
+    else{
+addBookButton.textContent="Add Book";
+form.innerHTML="";
+    }
+});
+
+//adding book info in array
+const btn=document.querySelector('.addBook');
+
+form.addEventListener('submit',()=>{
+    const bookName=document.querySelector('#bookName');
+    const authorName=document.querySelector('#authorName');
+    const pages=document.querySelector('#pages');
+    const markRead=document.querySelector('#markRead');
+    addBookToLibrary(new book(authorName.value,bookName.value,pages.value,markRead.checked));
+    displayBooks();
+    console.log(bookName.value);
+    console.log(authorName.value);
+    console.log(pages.value);
+    console.log(markRead.checked);
+});
